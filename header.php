b6b8388ae7bae16bf34ce8d7863cdb97c8187f04
zhurof <41473707+zhurof@users.noreply.github.com>
2019-12-03 16:14:26 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Верстка</title>
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no" />	
+	<link rel="stylesheet" href="css/fonts.css" />	
+	<link rel="stylesheet" href="css/font-awesome.css" />
+	<link rel="stylesheet" href="libs/slick/slick.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<header class="header">
+		<div class="wrapper">
+			<div class="header__topline">
+				<i class="icon-thumb-down-o"></i>
+			</div>
+		</div>
+	</header>
